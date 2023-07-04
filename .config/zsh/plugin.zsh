af55c9c57f0618f2a0163f58bdf2a1dcb1a65bc7
0xm4n <37541366+0xm4n@users.noreply.github.com>
2023-07-03 20:22:07 -0700

    Create plugin.zsh

---
+++ .config/zsh/plugin.zsh
@@ -0,0 +1,11 @@
+# ZSH plugins
+
+source $HOME/.config/zsh/zsh-autosuggestions/zsh-autosuggestions.zsh
+source $HOME/.config/zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+
+ZSH_AUTOSUGGEST_STRATEGY=(history completion)
+
+# remove highlight underline
+(( ${+ZSH_HIGHLIGHT_STYLES} )) || typeset -A ZSH_HIGHLIGHT_STYLES
+ZSH_HIGHLIGHT_STYLES[path]=none
+ZSH_HIGHLIGHT_STYLES[path_prefix]=none
