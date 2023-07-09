e63fc55b87b06c611ca3122b42424f6845b105c5
0xm4n <37541366+0xm4n@users.noreply.github.com>
2023-07-09 14:53:48 -0700

    Update misc.lua

--- .config/nvim/lua/plugins/misc.lua
+++ .config/nvim/lua/plugins/misc.lua
@@ -1,5 +1,3 @@
--- require("symbols-outline").setup()
-
 -- indent_blankline
 require("indent_blankline").setup {
     -- for example, context is off by default, use this to turn it on
@@ -9,9 +7,7 @@ require("indent_blankline").setup {
 
 -- Auto close Quickfix
 vim.cmd [[
-aug QFClose
-au!
-au WinEnter * if winnr('$') == 1 && &buftype == "quickfix"|q|endif
+aug QFClose au! au WinEnter * if winnr('$') == 1 && &buftype == "quickfix"|q|endif
 aug END
 ]]
 
@@ -37,4 +33,39 @@ if has("persistent_undo")
 endif
 ]]
 
+-- Fidget
 require"fidget".setup{}
+
+-- Copilot
+vim.cmd [[
+let g:copilot_filetypes = {
+      \ '*': v:false,
+      \ 'c': v:true,
+      \ 'python': v:true,
+      \ 'lua': v:true,
+      \ }
+]]
+
+-- Vista
+vim.cmd [[
+let g:vista_default_executive = 'nvim_lsp'
+let g:vista_sidebar_width = 50
+let g:vista_echo_cursor = 0
+let g:vista_disable_statusline = 1
+let g:vista#renderer#enable_icon = 0
+let g:vista_blink = [0, 0]
+]]
+vim.api.nvim_create_autocmd({ "BufEnter" }, {
+	pattern = { "*" },
+	callback = function()
+		local is_only_one_win = vim.fn.winnr("$") == 1
+		local is_vista_open = vim.fn["vista#sidebar#IsOpen"]() == 1
+
+		if is_only_one_win and is_vista_open then
+			vim.cmd("silent! q!") end end,
+})
+
+-- Tmux Navigator
+vim.cmd [[
+let g:tmux_navigator_no_mappings = 1
+]]
