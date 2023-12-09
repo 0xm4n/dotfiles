0180e778e63cf21b82f842b5a7b4026f7b07ac89
0xm4n <37541366+0xm4n@users.noreply.github.com>
2023-12-08 16:06:28 -0800

    Update nvimtree.lua

--- .config/nvim/lua/plugins/nvimtree.lua
+++ .config/nvim/lua/plugins/nvimtree.lua
@@ -2,8 +2,17 @@
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
--- empty setup using defaults
-require("nvim-tree").setup()
+require("nvim-tree").setup({
+	update_focused_file = {
+		enable = true,
+	},
+	actions = {
+		open_file = {
+			quit_on_open = true,
+		},
+	},
+})
+
 
 vim.api.nvim_create_autocmd('BufEnter', {
     command = "if winnr('$') == 1 && bufname() == 'NvimTree_' . tabpagenr() | quit | endif",
