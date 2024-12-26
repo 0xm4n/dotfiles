f63a6c0b86ca278d0e029f971f476c55f72d4a2f
0xm4n <37541366+0xm4n@users.noreply.github.com>
2024-12-25 16:31:30 -0800

    add nvim-treesitter-context

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -3,10 +3,13 @@ return {
 		"nvim-treesitter/nvim-treesitter",
 		version = false,
 		build = ":TSUpdate",
-		dependencies = "nvim-treesitter/nvim-treesitter-textobjects",
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter-textobjects",
+			"nvim-treesitter/nvim-treesitter-context",
+		},
 		config = function ()
 			require'nvim-treesitter.configs'.setup {
-				ensure_installed = { "c", "cpp", "lua" },
+				-- ensure_installed = { "c", "cpp", "lua" },
 				sync_install = false,
 				auto_install = false,
 				ignore_install = {},
@@ -58,7 +61,19 @@ return {
 				},
 			}
 			require("nvim-treesitter.install").prefer_git = true
+			require'treesitter-context'.setup{
+				enable = true,
+				multiwindow = false,
+				max_lines = 0,
+				min_window_height = 0,
+				line_numbers = true,
+				multiline_threshold = 20,
+				trim_scope = 'outer',
+				mode = 'cursor',
+				separator = nil,
+				zindex = 20,
+				on_attach = nil,
+			}
 		end,
 	},
 }
-
